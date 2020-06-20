4af0ff0b3538e99b709fe407a325265ac129cfb1
mbanakova <63152399+mbanakova@users.noreply.github.com>
2020-06-20 15:14:41 +0300

    main page

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+function css_style(done) {
+
+    gulp.src('./scss/style.scss')
+    .pipe(sass({
+        errorLogToConsole: true,
+        outputStyle: 'compressed'
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css/'));
+
+    done();
+}
+
+gulp.task(css_style);
+
+// exports.default = defaultSomeTask;
